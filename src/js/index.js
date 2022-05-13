7cd05f16491b5744189fb2960da3ffcf2ad7e7a0
Gabriel Quevedo <gquevedo1106@gmail.com>
2022-05-13 20:41:02 -0300

    add product filter

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,12 @@
-window.onload = () => {
-    console.log("Ola mentorados baum ?");
-    const containerLista = document.getElementById("lista-produtos")
+let produtos = [];
+const containerLista = document.getElementById("lista-produtos");
 
+window.onload = () => {
     // essa função espera
     async function pegaProdutos() {
         const response = await fetch("https://bendevoficial.com/api/mentoria/produtos").then((res) => {
             return res.json();
         });
-        console.log(response)
         response.map((produto) => {
             containerLista.innerHTML += `<div class="produto_item">
                                             <img src="${produto.productImage}">
@@ -16,7 +15,27 @@ window.onload = () => {
                                             </div>
                                         </div>`
         });
+        produtos = response;
     }
+    pegaProdutos();
+}
 
-    pegaProdutos()
+function filtrar() {
+    let valor = document.getElementById("busca").value;
+    const filtrados = [];
+    produtos.map((produto, index) => {
+        if (produto.productName.includes(valor)) {
+            filtrados.push(produto);
+        }
+    })
+
+    containerLista.innerHTML = "";
+    filtrados.map((produto) => {
+        containerLista.innerHTML += `<div class="produto_item">
+                                        <img src="${produto.productImage}">
+                                        <div class="produto_item_text">
+                                            <h2>${produto.productName}</h2>
+                                        </div>
+                                    </div>`
+    });
 };
